bb198f18b037bcf4e1fc931a812612f42f43c815
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-02-17 19:30:19 +0100

    refactor: fetch for update every 5 minutes

--- run.ts
+++ run.ts
@@ -1,66 +1,50 @@
 if (import.meta.main) {
   const fetchVersion = async (): Promise<string | undefined> => {
     try {
-      const res = await fetch('https://apiland.deno.dev/v2/modules/$name')
+      const res = await fetch('https://drgn.azury.dev/$name')
   
       if (res.ok) {
         const json = await res.json()
   
-        return json.latest_version
+        return json ? json.latest_version : ''
       }
-    // deno-lint-ignore no-empty
-    } catch (_err) {}
+    } catch (_err) {
+      return undefined
+    }
   }
 
   const item = localStorage.getItem('$url')
 
-  if (item && (Date.now() - Number(item.split(':')[1]) >= 1800000)) { // refetch every 30 minutes
-    Deno.run({ cmd: ['deno', 'eval', '--location', 'https://azury.dev/drgn_$location', `
-      try {
-        const res = await fetch('https://apiland.deno.dev/v2/modules/$name')
+  if (!item || item && (Date.now() - Number(item.split(':')[1]) >= 5 * 60000)) { // refetch every 5 minutes
+    const latestVersion = await fetchVersion()
 
-        if (res.ok) {
-          const json = await res.json()
-
-          localStorage.setItem('$url', json.latest_version + ':' + Date.now().toString())
-        }
-      } catch (_err) {}
-    `] })
-
-    Deno.env.set('__drgn-last_checked', item.split(':')[1])
-    Deno.env.set('__drgn-url', btoa('$url'))
-    Deno.env.set('__drgn-location', btoa('https://azury.dev/drgn_$location'))
-    Deno.env.set('__drgn-version', item.split(':')[0])
-    
-    const mod = await import('$url'.replace('$version', item.split(':')[0]))
-
-    mod.default()
-  } else if (item) {
-    Deno.env.set('__drgn-last_checked', item.split(':')[1])
-    Deno.env.set('__drgn-url', btoa('$url'))
-    Deno.env.set('__drgn-location', btoa('https://azury.dev/drgn_$location'))
-    Deno.env.set('__drgn-version', item.split(':')[0])
-
-    const mod = await import('$url'.replace('$version', item.split(':')[0]))
-
-    mod.default()
-  } else {
-    const version = await fetchVersion()
-
-    if (!version)
+    if (!latestVersion)
       throw new Error('cannot fetch version')
 
     const time = Date.now()
 
-    localStorage.setItem('$url', `${version}:${time}`)
-
+    localStorage.setItem('$url', `${latestVersion}:${time}`)
+  
     Deno.env.set('__drgn-last_checked', time.toString())
     Deno.env.set('__drgn-url', btoa('$url'))
     Deno.env.set('__drgn-location', btoa('https://azury.dev/drgn_$location'))
-    Deno.env.set('__drgn-version', version)
+    Deno.env.set('__drgn-version', latestVersion)
+      
+    const mod = await import('$url'.replace('$version', latestVersion))
+  
+    , exitCode = typeof mod.default === 'function' ? await mod.default() : await mod.run()
+  
+    Deno.exit(exitCode)
+  } else {
+    Deno.env.set('__drgn-last_checked', item.split(':')[1])
+    Deno.env.set('__drgn-url', btoa('$url'))
+    Deno.env.set('__drgn-location', btoa('https://azury.dev/drgn_$location'))
+    Deno.env.set('__drgn-version', item.split(':')[0])
 
-    const mod = await import('$url'.replace('$version', version))
+    const mod = await import('$url'.replace('$version', item.split(':')[0]))
 
-    mod.default()
+    , exitCode = typeof mod.default === 'function' ? await mod.default() : await mod.run()
+
+    Deno.exit(exitCode)
   }
 }
